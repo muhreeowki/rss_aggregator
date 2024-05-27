805407d3f4bc8067e5cf7b2fcf1eebb16b9f2c09
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-05-27 13:40:00 +0300

    Added error handling. Completed Boilerplate.

--- json.go
+++ json.go
@@ -6,6 +6,21 @@ import (
 	"net/http"
 )
 
+func respondWithError(writer http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Println("Responding with 5xx error: ", msg)
+	}
+
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(writer, code, errResponse{
+		Error: msg,
+	})
+}
+
+// Converts an object into a JSON string and sends that string in the http response body.
 func respondWithJSON(writer http.ResponseWriter, code int, payload interface{}) {
 	data, err := json.Marshal(payload)
 	if err != nil {
