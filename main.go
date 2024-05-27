eb8b19dc961b7439504ec32746c97afe1e6fdfce
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-05-27 10:56:43 +0300

    Setup server instance

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello world")
+	godotenv.Load(".env")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT not found in env.")
+	}
+
+	router := chi.NewRouter()
+
+	// Enable Cors
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"LINK"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	// Setup server http instance
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	// Start server
+	fmt.Printf("Server listening on PORT %v...", port)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
